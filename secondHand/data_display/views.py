552646b8985799fa2b836812dccc6c43e541c072
yorkyi <yiyusheng.hust@gmail.com>
2017-07-13 23:57:56 +0800

    databases

--- secondHand/data_display/views.py
+++ secondHand/data_display/views.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from data_display.models import Sh0713
+from data_display.models import Secondhand
 
 def data_display(request):
-    item_list = Sh0713.objects.order_by('-id')[:100]
+    item_list = Secondhand.objects.order_by('-create_time')[:500]
     return(render(request,'data_display/data_display.html',{'list':item_list}))
