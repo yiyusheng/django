a331646f29345c2ee71f649faa5a168b01bb5f7b
yiyusheng <yiyusheng@york.(none)>
2017-07-13 22:40:23 -0400

    update views.py to make all operation in view instead of templeate

--- secondHand/data_display/views.py
+++ secondHand/data_display/views.py
@@ -3,7 +3,21 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from data_display.models import Secondhand
+from datetime import datetime,timedelta
 
 def data_display(request):
-    item_list = Secondhand.objects.order_by('-create_time')[:500]
-    return(render(request,'data_display/data_display.html',{'list':item_list}))
+    # extract data
+    enddate = datetime.utcnow()
+    startdate = enddate + timedelta(days=-1)
+    item_list = Secondhand.objects.filter(create_time__range=[startdate,enddate]).order_by('-create_time')
+
+    # process
+    len_list = len(item_list)
+    for i in range(len_list):
+        item_list[i].create_time += timedelta(hours=+8)
+        item_list[i].ext2 = item_list[i].ext2==None and '-1' or item_list[i].ext2
+
+    # return
+    return(render(request,'data_display/data_display.html',
+        {'item_list':item_list,
+         'len_list': len_list}))
