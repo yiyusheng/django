76a22077d024be79a373c4e4aa1c7a17dc26a5d5
yorkyi <yiyusheng.hust@gmail.com>
2017-08-19 21:17:19 +0800

    UPDATE: advertiser filter

--- secondHand/show/views.py
+++ secondHand/show/views.py
@@ -11,28 +11,26 @@ def show(request):
     # Data proprepare
     now = datetime.utcnow()
     day1ago = now + timedelta(days=-1)
-    webname = Secondhand.objects.values('webname').distinct()
+    day3ago = now + timedelta(days=-3)
     seller_update_time = Advertiser.objects.all().aggregate(Max('update_time'))
+
+    webname = Secondhand.objects.values('webname').distinct()
     webnameList = [i.values()[0] for i in list(webname)]
-    maxItems = 500 
+
+    maxItems = 2000 
     customList = ['mac','surface','xbox',
             'ps4','kindle','kpw','ipad',
             'thinkpad','iphone7','moto']
     
-    # extract data 
+    # Get data 
     getDict = request.GET
     getWebname = len(getDict)>0 and set(webnameList).intersection(getDict.keys()) or ''
     keywords = (len(getDict)>0 and 'keywords' in getDict) and getDict['keywords'] or ''
-#    keywords = keywords.strip()
     uname = (len(getDict)>0 and 'uname' in getDict) and getDict['uname'] or ''
-    so = Secondhand.objects.extra(select={'is_ad':
-            '''SELECT * FROM secondHand WHERE NOT EXIST 
-            (SELECT 1 FROM advertiser ad WHERE ad.uname=sh.uname AND ad.webname=sh.webname'''})
-    so = Secondhand.objects.extra(
-            tables = ['advertiser'],
-            where = ['advertiser.uname=secondHand.uname or advertiser.webname=secondHand.webname'],
-            )
-    so = Secondhand.objects.filter(advertiser=0)
+
+    # Extract data
+    ad = Advertiser.objects.filter(update_time__range=[day3ago,now]).values_list('uname',flat=True)
+    so = Secondhand.objects.exclude(uname__in=ad)
     
     if len(keywords)+len(getWebname)+len(uname)==0 or len(getDict)==0:
         item_list = so.filter(create_time__range=[day1ago,now]).order_by('-time')[:maxItems]
