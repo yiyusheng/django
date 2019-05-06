4d7ef664ed7da976bb20e0c9d3a4f692889fc098
yiyusheng@vps <yiyusheng.hust@gmail.com>
2019-05-06 00:52:31 +0000

    0506:paginator

--- Secondhand/web.py
+++ Secondhand/web.py
@@ -7,7 +7,33 @@ from datetime import datetime,timedelta
 from django.db.models import Max,Q
 from django.utils import timezone
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-import operator
+import operator,re
+
+
+'''
+#from django.template import Library, Node, resolve_variable
+register = Library()
+class AddGetParameter(Node):
+    def __init__(self, values):
+        self.values = values
+        
+    def render(self, context):
+        req = resolve_variable('request', context)
+        params = req.GET.copy()
+        for key, value in self.values.items():
+            params[key] = value.resolve(context)
+        return '?%s' %  params.urlencode()
+
+
+@register.tag
+def add_get(parser, token):
+    pairs = token.split_contents()[1:]
+    values = {}
+    for pair in pairs:
+        s = pair.split('=', 1)
+        values[s[0]] = parser.compile_filter(s[1])
+    return AddGetParameter(values)
+'''
 
 def secondhand(request):
     # Data proprepare
@@ -80,10 +106,15 @@ def secondhand(request):
     except EmptyPage:
         item_list = paginator.page(paginator.num_pages)
 
+    # get last url
+    last_url = request.get_full_path()
+    last_url = re.sub('page\%3D\d+','',last_url)
+
     # return
     return(render(request,'secondhand.html',
         {'item_list':item_list,
          'len_list': len(item_list),
+         'last_url': last_url,
          'validSite': validWebList,
          'invalidSite': invalidWebList,
          'server_time':now,
